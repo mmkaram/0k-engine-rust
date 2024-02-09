4a5c4ac7ba518230441cdeb6fb2d29aa042a02ed
mmkaram <mahdykaram@outlook.com>
2024-02-08 16:59:34 -0800

    added main code

--- src/engine.rs
+++ src/engine.rs
@@ -1,12 +1,74 @@
-use chess::{Board, ChessMove, MoveGen, Square};
+use chess::{Board, ChessMove, Color, MoveGen, Piece};
 
-pub fn get_best_move(board: Board) -> ChessMove {
-    return ChessMove::new(Square::E2, Square::E5, None);
+use crate::piece_values;
+
+const PAWN_VALUE: f64 = 100.0;
+const BISHOP_VALUE: f64 = 300.0;
+const KNIGHT_VALUE: f64 = 300.0;
+const ROOK_VALUE: f64 = 500.0;
+const QUEEN_VALUE: f64 = 900.0;
+
+pub fn get_best_move(board: Board) -> Option<ChessMove> {
+    let mut test_board = board;
+    // let mut best_move;
+    let mut best_move: Option<ChessMove> = None;
+    let mut best_score = -f64::INFINITY;
+    let moves = MoveGen::new_legal(&board);
+
+    for a in moves {
+        test_board = test_board.make_move_new(a);
+        if eval_pos(test_board) < best_score {
+            best_move = Some(a);
+            best_score = eval_pos(test_board);
+        }
+    }
+
+    match best_move {
+        Some(chess_move) => {
+            // Use chess_move here...
+            return Some(chess_move);
+        }
+        None => {
+            // Handle the case where best_move was not set...
+            println!("couldn't find move");
+            return None;
+        }
+    }
 }
 
 fn eval_pos(board: Board) -> f64 {
-    return 2.7;
+    let mut final_score: f64 = 0.0;
+
+    for square in chess::ALL_SQUARES {
+        // Find a piece on the square and its side
+        let piece: Option<Piece> = board.piece_on(square);
+        let piece_side: Option<Color> = board.color_on(square);
+
+        // Get the side multiplier
+        let side_multiplier: f64 = match piece_side {
+            Some(Color::White) => 1.0,
+            Some(Color::Black) => -1.0,
+            None => 0.0,
+        };
+
+        // Add the piece's values * its side to the final score
+        if let Some(piece) = piece {
+            final_score += match piece {
+                Piece::Pawn => (PAWN_VALUE + piece_values::WHITE_PAWN.get(square.to_index()).unwrap()) * side_multiplier,
+                Piece::Knight => (KNIGHT_VALUE + piece_values::KNIGHT.get(square.to_index()).unwrap()) * side_multiplier,
+                Piece::Bishop => (BISHOP_VALUE + piece_values::BISHOP.get(square.to_index()).unwrap()) * side_multiplier,
+                Piece::Rook => (ROOK_VALUE + piece_values::ROOK.get(square.to_index()).unwrap()) * side_multiplier,
+                Piece::Queen => (QUEEN_VALUE /* + piece_values::QUEEN.get(square.to_index()).unwrap() */) * side_multiplier,
+                Piece::King => 0.0,
+            };
+        } else {
+            // println!("No piece")
+        }
+    }
+    // println!("Score: {}", final_score);
+    return final_score;
 }
+
 fn get_moves(board: Board) -> MoveGen {
     let movegen: MoveGen = MoveGen::new_legal(&board);
     return movegen;
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use chess::{Board, BoardStatus, ChessMove, Color};
 use std::io::{self, Write};
 
 mod engine;
+mod piece_values; // Declare the piece_values module
 
 fn main() {
     // init board, human player color
@@ -11,7 +12,6 @@ fn main() {
 
     // init human move and engine move
     let mut input = String::new();
-    let mut best_move: ChessMove;
 
     // while the there are still moves to be made
     // (not a checkmate or a stalemate)
@@ -24,10 +24,32 @@ fn main() {
             io::stdin()
                 .read_line(&mut input)
                 .expect("Failed to read line");
+            // println!("You entered: {}", input);
+
+            // attempt to take human move and play it if from_san() is successful
+            // throw error if from_san() fails
+            match ChessMove::from_san(&board, &input.trim()) {
+                Ok(chess_move) => {
+                    board = board.make_move_new(chess_move);
+                    println!("Player played: {}", chess_move.to_string());
+                }
+                Err(parse_error) => {
+                    // Failed to parse the chess move
+                    println!("Error: {}", parse_error);
+                    // Handle the error appropriately, such as displaying an error message or taking corrective action
+                }
+            }
         } else {
             // if it's the engine's move, play the engine's best move
-            best_move = engine::get_best_move(board);
-            board = board.make_move_new(best_move);
+            match engine::get_best_move(board) {
+                Some(best_move) => {
+                    board = board.make_move_new(best_move);
+                    println!("Engine played: {}", best_move.to_string());
+                } None => {
+                    panic!("Couldn't find a good move?");
+                }
+            }
+
         }
     }
 }
